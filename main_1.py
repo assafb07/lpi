3297a8fedd81808599f49a3f9d810eec44afba1d
1200 <1200km@gmail.com>
2023-01-07 10:48:03 +0200

    popup window not closes after second circle of loop

--- main_1.py
+++ main_1.py
@@ -89,57 +89,79 @@ def question(loop_number):
             body.append(chars[counter]+i)
             counter += 1
     return num, header, body, RIGHT_ANSWERC
+
 class BoxApp(App):
     """Main class"""
     answer_check = ""
     def callback(self, instance):
         """call back button function"""
-        self.answer += (instance.text)[0]
+        if len(self.answer) < len(RIGHT_ANSWERC):
+            self.answer += (instance.text)[0]
+            return self.answer
+        print("maximum " +str(len(RIGHT_ANSWERC))+" answers")
+        return self.answer
     def submit(self, instance):
         """submit function"""
         if self.answer == RIGHT_ANSWERC:
             answer_check ="Right!"
             print(answer_check)
-        else:
-            answer_check = "Wrong!"
-            print(answer_check)
+            self.popup.dismiss()
+            return 
+        answer_check = "Wrong!"
+        print(answer_check)
+        self.popup.dismiss()
+        return 
+
     def build(self):
         """build function"""
-        main_boxlayout = BoxLayout(orientation='vertical')
-        self.answer = ""
-        num, header, body, right_answ = question(VAR_NUMBER)
-        main_boxlayout.add_widget(Label(text = str(num))) #question number
-        main_boxlayout.add_widget(Label(text = str(header))) #question header
-        main_boxlayout.add_widget(Label(text = str(right_answ))) #right answer
-        button1 = (Button(text = str(body[0])))
-        button1.bind(on_press = self.callback)
-        button2 = (Button(text = str(body[1])))
-        button2.bind(on_press = self.callback)
-        button3 = (Button(text = str(body[2])))
-        button3.bind(on_press = self.callback)
-        button4 = (Button(text = str(body[3])))
-        button4.bind(on_press = self.callback)
-        button5 = (Button(text = str(body[4])))
-        button5.bind(on_press = self.callback)
-        button6 = (Button(text = "Submit" ))
-        main_boxlayout.add_widget(button1)
-        main_boxlayout.add_widget(button2)
-        main_boxlayout.add_widget(button3)
-        main_boxlayout.add_widget(button4)
-        main_boxlayout.add_widget(button5)
-        main_boxlayout.add_widget(button6)
-        popup = Popup(title ='Test popup', disabled=False, content = main_boxlayout)
-        popup.open()
-        button6.bind(on_press = self.submit)
-def random_e(rangenum):
-    """random questions function"""
-    global VAR_NUMBER
-    rangelist = list(range(1,rangenum+1))
-    random.shuffle(rangelist)
-    filllist = [17, 40, 56, 63]
-    for i in rangelist:
-        if i in filllist:
-            continue
-        VAR_NUMBER = i
-        BoxApp().run()
-random_e(4)
+        #main_boxlayout = BoxLayout(orientation='vertical')
+        open_button = Button(text = "Open exam", size_hint = (0.2, 0.2),
+                             pos_hint = {"center_x":0.5, "center_y":0.5},
+                             on_press = self.open_popup_window)
+        return open_button
+    def open_popup_window(self,obj):
+        for i in range(14):
+            main_boxlayout = BoxLayout(orientation='vertical')
+            self.answer = ""
+            num, header, body, right_answ = question(i)
+            main_boxlayout.add_widget(Label(text = str(num))) #question number
+            main_boxlayout.add_widget(Label(text = str(header))) #question header
+            main_boxlayout.add_widget(Label(text = str(right_answ))) #right answer
+            button1 = (Button(text = str(body[0])))
+            button1.bind(on_press = self.callback)
+            button2 = (Button(text = str(body[1])))
+            button2.bind(on_press = self.callback)
+            button3 = (Button(text = str(body[2])))
+            button3.bind(on_press = self.callback)
+            button4 = (Button(text = str(body[3])))
+            button4.bind(on_press = self.callback)
+            button5 = (Button(text = str(body[4])))
+            button5.bind(on_press = self.callback)
+            button6 = (Button(text = "Submit" ))
+            button6.bind(on_press = self.submit)
+            main_boxlayout.add_widget(button1)
+            main_boxlayout.add_widget(button2)
+            main_boxlayout.add_widget(button3)
+            main_boxlayout.add_widget(button4)
+            main_boxlayout.add_widget(button5)
+            main_boxlayout.add_widget(button6)
+            self.popup = Popup(title ='Test popup',
+                               content = main_boxlayout, auto_dismiss = False)
+            self.popup.open()
+            
+
+
+if __name__ == "__main__":
+    BoxApp().run()
+#def random_e(rangenum):
+#    """random questions function"""
+#    global VAR_NUMBER
+#    rangelist = list(range(1,rangenum+1))
+#    random.shuffle(rangelist)
+#    filllist = [17, 40, 56, 63]
+#    for i in rangelist:
+#        if i in filllist:
+#            continue
+#        VAR_NUMBER = i
+#        open_popup_window().run()
+#random_e(4)
