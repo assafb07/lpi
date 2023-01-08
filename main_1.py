dbdeca7fcad699183284911055e93fc5cc006762
1200 <1200km@gmail.com>
2023-01-08 14:06:28 +0200

    Can't u[date variable "right_answer" in "press" function. pylint 9.83

--- main_1.py
+++ main_1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
- #pylint:disable=E1101
+#pylint:disable=E1101,E0611
 """LPI exam simulator Application for Android"""
 import random
 from kivy.app import App
@@ -7,6 +7,7 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 with open('qst.txt', "r", encoding="utf-8") as file:
     text_lines = [line[:-1] for line in file]
 
@@ -67,8 +68,7 @@ ra_pl = [[1,5],[2],[4],[4],[2],[4],[5],[2,3,5],[5],[5],[5],[1],[1],[2],
 
 def question(loop_number):
     """function creates question"""
-    global RIGHT_ANSWERC
-    RIGHT_ANSWERC = ""
+    right_answerc = ""
     loop_number = loop_number - 1
     num = "Question:" + nums(text_lines)[loop_number]
     header = headers(text_lines)[loop_number]
@@ -82,83 +82,68 @@ def question(loop_number):
     counter = 0
     for i in bodyt:
         if i in ra_answ:
-            RIGHT_ANSWERC += chars[counter]
+            right_answerc += chars[counter]
             body.append(chars[counter]+i)
             counter += 1
         else:
             body.append(chars[counter]+i)
             counter += 1
-    return num, header, body, RIGHT_ANSWERC
+    return num, header, body, right_answerc
 
 class BoxApp(App):
     """Main class"""
-    answer_check = ""
-    def callback(self, instance):
-        """call back button function"""
-        if len(self.answer) < len(RIGHT_ANSWERC):
-            self.answer += (instance.text)[0]
-            return self.answer
-        print("maximum " +str(len(RIGHT_ANSWERC))+" answers")
-        return self.answer
-    def submit(self, instance):
-        """submit function"""
-        if self.answer == RIGHT_ANSWERC:
-            answer_check ="Right!"
-            print(answer_check)
-            self.popup.dismiss()
-            return
-        answer_check = "Wrong!"
-        print(answer_check)
-        self.popup.dismiss()
-        return
-
+    right_answer = StringProperty("string")
     def build(self):
         """build function"""
-        #main_boxlayout = BoxLayout(orientation='vertical')
         open_button = Button(text = "Open exam", size_hint = (0.2, 0.2),
                              pos_hint = {"center_x":0.5, "center_y":0.5},
                              on_press = self.open_popup_window)
         return open_button
-    def open_popup_window(self,obj):
-        for i in range(14):
-            main_boxlayout = BoxLayout(orientation='vertical')
-            self.answer = ""
-            num, header, body, right_answ = question(i)
-            main_boxlayout.add_widget(Label(text = str(num))) #question number
-            main_boxlayout.add_widget(Label(text = str(header))) #question header
-            main_boxlayout.add_widget(Label(text = str(right_answ))) #right answer
-            button1 = (Button(text = str(body[0])))
-            button1.bind(on_press = self.callback)
-            button2 = (Button(text = str(body[1])))
-            button2.bind(on_press = self.callback)
-            button3 = (Button(text = str(body[2])))
-            button3.bind(on_press = self.callback)
-            button4 = (Button(text = str(body[3])))
-            button4.bind(on_press = self.callback)
-            button5 = (Button(text = str(body[4])))
-            button5.bind(on_press = self.callback)
-            button6 = (Button(text = "Submit" ))
-            button6.bind(on_press = self.submit)
-            main_boxlayout.add_widget(button1)
-            main_boxlayout.add_widget(button2)
-            main_boxlayout.add_widget(button3)
-            main_boxlayout.add_widget(button4)
-            main_boxlayout.add_widget(button5)
-            main_boxlayout.add_widget(button6)
-            self.popup = Popup(title ='Test popup',
-                               content = main_boxlayout, auto_dismiss = False)
-            self.popup.open()
+    def open_popup_window(self, instance):
+        """open popup function"""
+        def random_range(range_num):
+            """open random range function"""
+            rangelist = list(range(range_num))
+            random.shuffle(rangelist)
+            filllist = [17, 40, 56, 63]
+            for i in rangelist:
+                if i in filllist:
+                    continue
+                main_boxlayout = BoxLayout(orientation='vertical')
+                num, header, body, self.right_answer = question(i)
+                self.popup = Popup(title ='Test popup',
+                                   content = main_boxlayout, auto_dismiss = False)
+                main_boxlayout.add_widget(Label(text = str(num))) #question number
+                main_boxlayout.add_widget(Label(text = str(header))) #question header
+                main_boxlayout.add_widget(Label(text = str(self.right_answer))) #right answer
+                main_label = (Label(text = "Your input"))
+                main_boxlayout.add_widget(main_label)
+                button1 = (Button(text = str(body[0])))
+                button1.bind(on_press = self.press)
+                button2 = (Button(text = str(body[1])))
+                button2.bind(on_press = self.press)
+                button3 = (Button(text = str(body[2])))
+                button3.bind(on_press = self.press)
+                button4 = (Button(text = str(body[3])))
+                button4.bind(on_press = self.press)
+                button5 = (Button(text = str(body[4])))
+                button5.bind(on_press = self.press)
+                button6 = (Button(text = "Next question" ))
+                button6.bind(on_release = self.popup.dismiss)
+                main_boxlayout.add_widget(button1)
+                main_boxlayout.add_widget(button2)
+                main_boxlayout.add_widget(button3)
+                main_boxlayout.add_widget(button4)
+                main_boxlayout.add_widget(button5)
+                main_boxlayout.add_widget(button6)
+                self.popup.open()
+        random_range(80)
+    def press(self, obj):
+        """button press function"""
+        print("self.a="+self.right_answer)
+        user_answer = str(obj.text)[0]
+        print(user_answer)
+        user_answer = ""
+        obj.text = self.right_answer
 if __name__ == "__main__":
     BoxApp().run()
-#def random_e(rangenum):
-#    """random questions function"""
-#    global VAR_NUMBER
-#    rangelist = list(range(1,rangenum+1))
-#    random.shuffle(rangelist)
-#    filllist = [17, 40, 56, 63]
-#    for i in rangelist:
-#        if i in filllist:
-#            continue
-#        VAR_NUMBER = i
-#        open_popup_window().run()
-#random_e(4)
